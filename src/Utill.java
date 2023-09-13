94ec948e832cc188448643d54da9110e0881f351
SaintArthurG <cazooklol@gmail.com>
2023-09-13 17:09:15 -0300

    REALIZEI UMA CONEXÃO COM UMA API DE CEP. APRENDI A INCLUIR UMA BIBLIOTECA EXTERNA "GSON" NO INTELLIJ, APRENDI UMAS NOVAS BIBLIOTECAS COMO "BufferedReader" E COMO TRANSFORMAR UM TEXTO JSON EM STRING NO JAVA

---
+++ src/Utill.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Utill {
+    public static String converteJsonEmString(BufferedReader bufferedReader) throws IOException {
+        String resposta, jsonEmString = "";
+        while ((resposta = bufferedReader.readLine()) != null) {
+            jsonEmString += resposta;
+        }
+        return jsonEmString;
+    }
+}

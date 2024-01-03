5bab2141090e9de6afa1b5cf157b36098b185a17
SaintArthurG <cazooklol@gmail.com>
2024-01-03 20:58:17 -0300

    add a function to do the print

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,20 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("Informe seu CEP: ");
         String cep = new Scanner(System.in).nextLine();
-        Endereco endereco = ServicoDeCep.buscaEnderecoPelo(cep);
+        Endereco(cep);
 
-        System.out.println("Logradouro: " + endereco.getLogradouro());
-        System.out.println("Bairro: " + endereco.getBairro());
-        System.out.println("Localidade: " + endereco.getLocalidade());
 
-        /* teste para fazer um novo commit */
+
+    }
+    public static void Endereco (String cep){
+        try {
+            Endereco endereco = ServicoDeCep.buscaEnderecoPelo(cep);
+            System.out.println("Logradouro: " + endereco.getLogradouro());
+            System.out.println("Bairro: " + endereco.getBairro());
+            System.out.println("Localidade: " + endereco.getLocalidade());
+        }
+        catch (Exception e){
+            System.out.println(e.getMessage());
+        }
     }
 }

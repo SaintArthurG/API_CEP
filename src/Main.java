6e1ee527e74df3c0770d87d8aef92bd524ebcfc4
SaintArthurG <cazooklol@gmail.com>
2024-01-03 21:07:31 -0300

    .

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("Informe seu CEP: ");
         String cep = new Scanner(System.in).nextLine();
+
         Endereco(cep);
 
 
